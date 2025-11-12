b13150962df31c4214227c567c2da7f50ce23d3a
Ofek Mossinson <ofekmoss@Ofeks-MacBook-Pro-2.local>
2025-11-12 20:18:35 +0200

    question 2

--- Algebra.java
+++ Algebra.java
@@ -13,7 +13,7 @@ public class Algebra {
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(-12,-3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
@@ -25,16 +25,29 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1++;
+		if (x2 < 0) {
+			for (int i = 0; i < -x2; i++) {
+				x1--;
+			}
+		} else {
+			for (int i = 0; i < x2; i++) {
+				x1++;
+			}
 		}
 		return x1;
 	}
 	
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1--;
+		if (x2 > 0) {
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
+			return x1;
+		} else {
+			for (int i = 0; i < -x2; i++) {
+				x1++;
+			}
 		}
 		return x1;
 	}
@@ -42,16 +55,22 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int result = 0;
-		for (int i = 0; i < x2; i++) {
-			result = plus(result, x1);
+		if (x2 > 0) {
+			for (int i = 0; i < x2; i++) {
+				result = plus(result, x1);
+			}
+		} else {
+			for (int i = 0; i < -x2; i++) {
+				result = minus(result, x1);
+			}
 		}
 		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		int result = x;
-		for (int i = 1; i < n; i++) {
+		int result = 1;
+		for (int i = 0; i < n; i++) {
 			result = times(result, x);
 		}
 		return result;
@@ -60,10 +79,31 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		int result = 0;
-		while (x1 >= x2) {
-			x1 = minus(x1, x2);
-			result = plus(result, 1);
-		}
+		if (x1 > 0) {
+			if (x2 > 0) {
+				while (x1 >= x2) {
+					x1 = minus(x1, x2);
+					result = plus(result, 1);
+				}
+			} else {
+				while (x1 >= -x2) {
+					x1 = minus(x1, -x2);
+					result = minus(result, 1);
+				}
+			}
+		} else {
+			if (x2 > 0) {
+				while (-x1 >= x2) {
+					x1 = minus(x1, -x2);
+					result = minus(result, 1);
+				}
+			} else {
+				while (x1 <= x2) {
+					x1 = plus(x1, -x2);
+					result = plus(result, 1);
+				}
+			}
+		} 
 		return result;
 	}
 
